a6e60c6952eef9ccb6e2db19fca0211c977a9d14
Max808S <rayydubb808@gmail.com>
2022-03-16 22:28:57 +0200

    add cg_categories.csv

--- bot/api_requests/coingecko/token_json.py
+++ bot/api_requests/coingecko/token_json.py
@@ -7,3 +7,11 @@ with open('CoinGecko Token API List - CoinGecko Token API List.csv', mode='r') a
     
 csv_keys = list(dict_from_csv.keys())
 csv_values = list(dict_from_csv.values())
+
+
+with open('CG Categories.csv', mode='r') as cg_categories_csv_data:
+    reader = csv.reader(cg_categories_csv_data)
+    dict_from_cg_categories_csv = {rows[0]:rows[1] for rows in reader}
+
+cg_categories_keys = list(dict_from_cg_categories_csv.keys())
+cg_categories_values = list(dict_from_cg_categories_csv.values())
