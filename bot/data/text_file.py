12adb982cab36ce059a85eae69b48dc4a10e0521
Max808S <rayydubb808@gmail.com>
2022-03-18 22:29:00 +0200

    add texts description

---
+++ bot/data/text_file.py
@@ -0,0 +1,101 @@
+# А file with all the necessary text commands for the bot to work
+
+
+help_text = """
+<b>CryptoApe Help Menu:</b>
+
+/coins - посмотреть монеты
+/categories - посмотреть категории
+/restart - перезагрузить бота"""
+
+
+coins_text = """
+Выберите монету, которую хотите посмотреть:
+"""
+
+
+trend_text = (
+    'Топ 7 самых популярных монет на CoinGecko по поиску '
+    'пользователей за последние 24 часа в порядке убывания популярности:'
+)
+
+
+categories_commands = """
+/smart_contract_platform <b>Smart Contract Platform</b>
+/binance_smart_chain <b>BNB Chain Ecosystem</b>
+/polygon_ecosystem <b>Polygon Ecosystem</b>
+/avalanche_ecosystem <b>Avalanche Ecosystem</b>
+/moonriver_ecosystem <b>Moonriver Ecosystem</b>
+/fantom_ecosystem <b>Fantom Ecosystem</b>
+/stablecoins <b>Stablecoins</b>
+/arbitrum_ecosystem	<b>Arbitrum Ecosystem</b>
+/near_protocol_ecosystem <b>Near Protocol Ecosystem</b>
+/decentralized_finance_defi	<b>Decentralized Finance (DeFi)</b>
+/exchange_based_tokens <b>Exchange-based Tokens</b>
+/centralized_exchange_token_cex	<b>Centralized Exchange Token</b>
+/xdai_ecosystem	<b>Gnosis Chain Ecosystem</b>
+/cosmos_ecosystem <b>Cosmos Ecosystem</b>
+/terra_ecosystem <b>Terra Ecosystem</b>
+/solana_ecosystem <b>Solana Ecosystem</b>
+/cardano_ecosystem <b>Cardano Ecosystem</b>
+/dot_ecosystem <b>Polkadot Ecosystem</b>
+/non_fungible_tokens_nft <b>Non-Fungible Tokens (NFT)</b>
+/meme_token	<b>Meme Tokens</b>
+/harmony_ecosystem <b>Harmony Ecosystem</b>
+/decentralized_exchange	<b>Decentralized Exchange Token</b>
+/governance	<b>Governance</b>
+/seigniorage <b>Seigniorage</b>
+/play_to_earn <b>Play To Earn</b>
+/metaverse <b>Metaverse</b>
+/gaming	<b>GameFi</b>
+/yield_farming <b>Yield Farming</b>
+/asset_backed_tokens <b>Asset-backed Tokens</b>
+/automated_market_maker_amm	<b>Automated Market Maker</b>
+/wrapped_tokens	<b>Wrapped-Tokens</b>
+/finance_banking <b>Finance / Banking</b>
+/privacy_coins <b>Privacy Coins</b>
+/internet_of_things_iot	<b>Internet of Things</b>
+/oracle	<b>Oracle</b>
+/lending_borrowing <b>Lending/Borrowing</b>
+/eth_2_0_staking <b>Eth 2.0 Staking</b>
+/storage <b>Storage</b>
+/decentralized_derivatives <b>Derivatives</b>
+/sports	<b>Sports</b>
+/olympus_pro <b>Olympus Pro</b>
+/masternodes <b>Masternodes</b>
+/yield_aggregator <b>Yield Aggregator</b>
+/analytics <b>Analytics</b>
+/celo_ecosystem	<b>Celo Ecosystem</b>
+/synthetic_assets <b>Synthetic Issuer</b>
+/yearn_yfi_partnerships_mergers	<b>Yearn Ecosystem</b>
+/oec_ecosystem <b>OEC Ecosystem</b>
+/decentralized_perpetuals <b>Perpetuals</b>
+/rebase_tokens <b>Rebase Tokens</b>
+/insurance <b>Insurance</b>
+/mev_protection	<b>MEV Protection</b>
+/zilliqa_ecosystem <b>Zilliqa Ecosystem</b>
+/prediction_markets	<b>Prediction Markets</b>
+/iotex_ecosystem <b>IoTeX Ecosystem</b>
+/social_money <b>Social Money</b>
+/decentralized_options <b>Options</b>
+/music <b>Music</b>
+/guild_scholarship <b>Guild and Scholarship</b>
+/daomaker_ecosystem	<b>DaoMaker Ecosystem</b>
+/synths	<b>Synths</b>
+/ohm_fork <b>Ohm Fork</b>
+/heco_chain_ecosystem <b>HECO Chain Ecosystem</b>
+/fan_token <b>Fan Token</b>
+/gambling <b>Gambling</b>
+/defi_index	<b>DeFi Index</b>
+/tourism <b>Tourism</b>
+/kardiachain_ecosystem <b>KardiaChain Ecosystem</b>
+/farming_as_a_service_faas	<b>Farming-as-a-Service (FaaS)</b>
+/aave_tokens <b>Aave Tokens</b>
+/tokenized_stock <b>Tokenized Stock</b>"""
+
+
+categories_text = (f"""
+Выберите категорию которую хотите посмотреть:
+
+{categories_commands}
+""")
