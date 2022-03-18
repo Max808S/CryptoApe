57db29096448c5739d242f4a1cc37ec0e202a32c
Max808S <rayydubb808@gmail.com>
2022-03-18 22:28:11 +0200

    rename variables

--- bot/api_requests/coingecko/token_json.py
+++ bot/api_requests/coingecko/token_json.py
@@ -3,10 +3,10 @@ import csv
 
 with open('CoinGecko Token API List - CoinGecko Token API List.csv', mode='r') as csv_data:
     reader = csv.reader(csv_data)
-    dict_from_csv = {rows[0]:rows[1] for rows in reader}
+    dict_from_tokens_csv = {rows[0]:rows[1] for rows in reader}
     
-csv_keys = list(dict_from_csv.keys())
-csv_values = list(dict_from_csv.values())
+csv_keys = list(dict_from_tokens_csv.keys())
+csv_values = list(dict_from_tokens_csv.values())
 
 
 with open('CG Categories.csv', mode='r') as cg_categories_csv_data:
