1887edfa209771da469c7ae6abcfefbad47f9d07
Max808S <rayydubb808@gmail.com>
2022-03-25 23:30:20 +0200

    updated logging

--- bot/utils/misc/logging.py
+++ bot/utils/misc/logging.py
@@ -1,24 +1,27 @@
 import logging
 from pathlib import Path
 
-# logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-#                     level=logging.INFO) # level=logging.DEBUG
-
 
 log_folder = Path.home().joinpath("logs")
 Path(log_folder).mkdir(parents=True, exist_ok=True)
-log_file = log_folder.joinpath("crypto_cat_tgbot.log")
+log_file = log_folder.joinpath("logs_tgbot.log")
+
 
 if not log_file.exists():
     open(log_file, "w").close()
 
+# FORMAT = f"[%(asctime)s] %(module)s - %(funcName)s [LINE:%(lineno)d] #%(levelname)-5s %(message)s"
+FORMAT = f"[%(asctime)s] [LINE:%(lineno)d] #%(levelname)-5s %(message)s"
+
+
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    format=FORMAT,
     level=logging.INFO,
-    handlers=[logging.FileHandler(log_file, mode="w+"), logging.StreamHandler()],
+    handlers=[logging.FileHandler(log_file, mode="w+"), logging.StreamHandler()]
 )
+
 
 logger = logging.getLogger(__name__)
 
 # coin notify 
-log = logging.getLogger('broadcast')
+# log = logging.getLogger('broadcast')
