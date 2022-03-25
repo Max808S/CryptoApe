6cd671a55ff3b23d70b40cead6066516574a6cab
Max808S <rayydubb808@gmail.com>
2022-03-25 23:15:04 +0200

    added config_reader, startup_notify, throttling

--- bot/bot.py
+++ bot/bot.py
@@ -1,70 +1,51 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.dispatcher.fsm.storage.memory import MemoryStorage
-# from aiogram.dispatcher.fsm.storage.redis import RedisStorage    # TODO
 
-from handlers.for_admins import admin_router
-from handlers.for_users import user_router, register_commands
-# from handlers.coingecko.coins import register_coins_commands
-from handlers.opensea.collections import register_opensea   # TODO
+from handlers import default_commands, admin_commands, coingecko_commands
 
-from utils.misc.logging import logging
-# from utils.notify_admins import on_startup_notify
 from utils.commands import set_bot_commands
+from utils.notify_admins import on_startup_notify
 
-# from middlewares.throttling import ThrottlingMiddleware   # TODO
-from data.config import BOT_TOKEN, ADMINS
+from data.config_reader import load_config
+from middlewares.throttling import ThrottlingMiddleware
 from magic_filter import F
-
-# # broadcaster TODO
-# from services.broadcaster import *
 
 
 async def main() -> None:
-    logging.basicConfig(level=logging.WARNING)
-
-    # Reading config file   TODO
-    # config = load_config()
+    # Reading config file
+    config = load_config()
 
     # Creating bot
-    bot = Bot(token=BOT_TOKEN, parse_mode="HTML") 
+    bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
 
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
 
-    # bot start admins notify   TODO
-    # await on_startup_notify(dp)
-
-    # Register handlers
-    register_commands(user_router)    # START / HELP
-    # register_opensea(default_router)    # TODO
-    # register_coins_commands(default_router)
-
     # Add admin filter to admin_router
-    admin_router.message.filter(F.chat.id == ADMINS)
+    admin_commands.admin_router.message.filter(F.chat.id == config.tg_bot.admin)
 
-    # Add routers to dispatcher
-    dp.include_router(admin_router)
-    dp.include_router(user_router)
+    # Register routers with handlers
+    dp.include_router(default_commands.user_router)
+    dp.include_router(admin_commands.admin_router)
+    dp.include_router(coingecko_commands.coingecko_router)
+    
+    # Register middleware for throttling
+    dp.message.middleware(ThrottlingMiddleware())
 
-    # Создание диспетчера и навешивание роутера     # TODO
-    # if config.bot.fsm_type == "redis":
-    #     storage = RedisStorage.from_url(
-    #         url=f"redis://default:{config.redis.password}@{config.redis.host}:{config.redis.port}",
-    #         connection_kwargs={"decode_responses": True, "db": config.redis.db}
-    #     )
-    # else:
-    #     storage = MemoryStorage()
-
-    # Register middlewares TODO
-    # dp.message.middleware(ThrottlingMiddleware()) 
-
+    # Notification for admins about the start of the bot TODO async def on_startup(dp): 
+    await on_startup_notify(bot)
+    
     # Register /-commands
     await set_bot_commands(bot)
 
     try:
-        await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+        await dp.start_polling(
+            bot, 
+            allowed_updates=dp.resolve_used_update_types()
+        ) # on_startup=on_startup
     finally:
         await bot.session.close()
 
