24b3cecb44c203c6c8f2ee20633b31dcbfe8985f
Max808S <rayydubb808@gmail.com>
2022-03-25 23:28:10 +0200

    keyboard update

--- bot/keypads/keyboards.py
+++ bot/keypads/keyboards.py
@@ -1,26 +1,69 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
 from aiogram.types.inline_keyboard_button import InlineKeyboardButton
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
-from aiogram.dispatcher.filters.callback_data import CallbackData   # TODO
+
+import secrets # testing
+
+
+def get_back_main_menu_keyboards():
+    """
+    Get /btc /ltc /idt inline buttons
+    """
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(text="Главное меню", callback_data="back_to_main_menu")
+        ]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+
+
+def get_extra_categories_keyboards():
+    """
+    Get extra categories inline buttons
+    """
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(text="Категории", callback_data="categories"),
+            InlineKeyboardButton(text="Главное меню", callback_data="back_to_main_menu")
+        ]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+
+
+def get_extra_coins_keyboards():
+    """
+    /btc /ltc /idt inline buttons
+    """
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(text="Назад", callback_data="coins"),
+            InlineKeyboardButton(text="Главное меню", callback_data="back_to_main_menu")
+        ]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
 
 
 def get_main_keyboard() -> None:
     """
     /start menu
     """
-    keyboard = [
-        [KeyboardButton(text='Монеты'), KeyboardButton(text='OpenSea'), KeyboardButton(text='Газ')],    # row 1
-        [KeyboardButton(text='Биржи'), KeyboardButton(text='Алерт'), KeyboardButton(text='Категории')]     # row 2        
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(text="Монеты", callback_data="coins"),
+            InlineKeyboardButton(text="Категории", callback_data="categories")
+        ],
+        [
+            InlineKeyboardButton(text="Тренд", callback_data="trend"),
+            InlineKeyboardButton(text=secrets.token_urlsafe(4), callback_data="test")
+        ]
     ]
-    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
 
 
 def get_categories_inline_keyboard() -> None:
     """
     Get /categories keyboard buttons
     """
-    inline_board = [
+    inline_keyboard = [
         [
             InlineKeyboardButton(text='Smart Contract Platform', callback_data='/smart-contract-platform'),
             InlineKeyboardButton(text='BNB Chain Ecosystem', callback_data='/binance-smart-chain')
@@ -32,43 +75,49 @@ def get_categories_inline_keyboard() -> None:
         [
             InlineKeyboardButton(text='Meme Tokens', callback_data='/meme-token'),
             InlineKeyboardButton(text='Metaverse', callback_data='/metaverse')
+        ],
+        [
+            InlineKeyboardButton(text="Назад", callback_data="back_to_main_menu")
         ]
     ]
-    return InlineKeyboardMarkup(inline_keyboard=inline_board)
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
 
 
 def get_coin_inline_keyboard() -> None:
     """
     Get /coin keyboard buttons
     """
-    inline_board = [
+    inline_keyboard = [
         [
-            InlineKeyboardButton(text='BTC', callback_data='/btc'), 
-            InlineKeyboardButton(text='ETH', callback_data='/eth'), 
-            InlineKeyboardButton(text='BNB', callback_data='/bnb'), 
-            InlineKeyboardButton(text='SOL', callback_data='/sol'), 
-            InlineKeyboardButton(text='DOT', callback_data='/dot')
+            InlineKeyboardButton(text='BTC', callback_data='btc'), 
+            InlineKeyboardButton(text='ETH', callback_data='eth'), 
+            InlineKeyboardButton(text='BNB', callback_data='bnb'), 
+            InlineKeyboardButton(text='SOL', callback_data='sol'), 
+            InlineKeyboardButton(text='DOT', callback_data='dot')
         ],
         [
-            InlineKeyboardButton(text='XRP', callback_data='/xrp'),
-            InlineKeyboardButton(text='ADA', callback_data='/ada'),
-            InlineKeyboardButton(text='LUNA', callback_data='/luna'),
-            InlineKeyboardButton(text='AVAX', callback_data='/avax'),
-            InlineKeyboardButton(text='DOGE', callback_data='/doge')
+            InlineKeyboardButton(text='XRP', callback_data='xrp'),
+            InlineKeyboardButton(text='ADA', callback_data='ada'),
+            InlineKeyboardButton(text='LUNA', callback_data='luna'),
+            InlineKeyboardButton(text='AVAX', callback_data='avax'),
+            InlineKeyboardButton(text='DOGE', callback_data='doge')
         ],
         [
-            InlineKeyboardButton(text='MATIC', callback_data='/matic'),
-            InlineKeyboardButton(text='LINK', callback_data='/link'),
-            InlineKeyboardButton(text='NEAR', callback_data='/near'),
-            InlineKeyboardButton(text='LTC', callback_data='/ltc'),
-            InlineKeyboardButton(text='TRX', callback_data='/trx')
+            InlineKeyboardButton(text='MATIC', callback_data='matic'),
+            InlineKeyboardButton(text='LINK', callback_data='link'),
+            InlineKeyboardButton(text='NEAR', callback_data='near'),
+            InlineKeyboardButton(text='LTC', callback_data='ltc'),
+            InlineKeyboardButton(text='TRX', callback_data='trx')
         ],
         [
-            InlineKeyboardButton(text='XLM', callback_data='/xlm'),
-            InlineKeyboardButton(text='VET', callback_data='/vet'),
-            InlineKeyboardButton(text='SAND', callback_data='/sand'),
-            InlineKeyboardButton(text='GALA', callback_data='/gala'),
-            InlineKeyboardButton(text='OASIS', callback_data='/oasis')
+            InlineKeyboardButton(text='XLM', callback_data='xlm'),
+            InlineKeyboardButton(text='VET', callback_data='vet'),
+            InlineKeyboardButton(text='SAND', callback_data='sand'),
+            InlineKeyboardButton(text='GALA', callback_data='gala'),
+            InlineKeyboardButton(text='OASIS', callback_data='oasis')
+        ],
+        [
+            InlineKeyboardButton(text="Назад", callback_data="back_to_main_menu")
         ]
-        ]
-    return InlineKeyboardMarkup(inline_keyboard=inline_board)
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
