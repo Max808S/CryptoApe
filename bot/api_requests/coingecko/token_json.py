c8442556f6d8067060896bfc8290a56f89ef157f
Max808S <rayydubb808@gmail.com>
2022-04-08 23:28:56 +0300

    added category_name_changer

--- bot/api_requests/coingecko/token_json.py
+++ bot/api_requests/coingecko/token_json.py
@@ -1,11 +1,12 @@
 import csv
+import aiohttp
 
 from csv import reader
 from itertools import zip_longest
 
 
 def name_changer():
-    with open("CoinGecko Token API List - CoinGecko Token API List.csv", mode="r") as csv_data:
+    with open("/Users/keyglock/Documents/python/CryptoCurrency/CoinGecko Token API List - CoinGecko Token API List.csv", mode="r") as csv_data:
         reader = csv.reader(csv_data)    
         token_id_list = [rows[0] for rows in reader]
         # token_symbol_list = [rows[1] for rows in reader] # TODO
@@ -19,12 +20,52 @@ def name_changer():
         write = csv.writer(file)
         write.writerow(("Id", "Extra"))
         write.writerows(export_data)
-    result = "[*] Original and new names are written to a new CSV file!"
+    result = "[*] The original and additional names are written to a new CSV file!"
     return result
 
 
+async def categories_list_changer() -> str:
+    """
+    List all categories and save to CSV with extra named categories
+    """
+    async with aiohttp.ClientSession() as session:
+        url = "https://api.coingecko.com/api/v3/coins/categories/list"
+        
+        async with session.get(url) as response:
+            categories_data = await response.json()
+        
+        categories_id = []
+        categories_name = []
+        
+        for item in categories_data:
+            category_id = item["category_id"]
+            category_name = item["name"]
+            categories_id.append(category_id)
+            categories_name.append(category_name)
+
+        extra_categories_id = [word.replace("-", "_") for word in categories_id]
+
+        data = [categories_id, extra_categories_id, categories_name]
+        export_data = zip_longest(*data, fillvalue = '')
+
+        with open('CG_categories_list.csv', 'w', encoding="ISO-8859-1", newline='') as file:
+            write = csv.writer(file)
+            write.writerow(("ID", "Extra", "Name"))
+            write.writerows(export_data)
+        result = "[*] The original and additional names are written to a new CSV file!"
+        return result
+
+
+with open('/Users/keyglock/Documents/python/CryptoCurrency/CG_categories_list.csv', mode='r') as cg_categories_csv_data:
+    reader = csv.reader(cg_categories_csv_data)
+    dict_from_cg_categories_csv = {rows[0]:rows[1] for rows in reader}
+
+cg_categories_keys = dict_from_cg_categories_csv.keys()
+cg_categories_values = list(dict_from_cg_categories_csv.values())
+
+
 # EXAMPLE: tron-eth - tron_eth
-with open("CG_token_list.csv", mode="r") as csv_data:
+with open("/Users/keyglock/Documents/python/CryptoCurrency/CG_token_list.csv", mode="r") as csv_data:
     reader = csv.reader(csv_data)
     dict_with_token_ids = {rows[0]:rows[1] for rows in reader}
 
@@ -35,7 +76,7 @@ cg_token_extra_name = list(dict_with_token_ids.values())
 
 
 # EXAMPLE: bitcoin - btc
-with open("CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv", mode="r") as csv_data:
+with open("/Users/keyglock/Documents/python/CryptoCurrency/CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv", mode="r") as csv_data:
     reader = csv.reader(csv_data)
     dict_from_tokens_csv = {rows[0]:rows[1] for rows in reader}
 
@@ -46,7 +87,7 @@ cg_tokens_values = list(dict_from_tokens_csv.values())
 
 
 # EXAMPLE: bitcoin - Bitcoin (name)
-with open('CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv', mode='r') as extra_csv_data:
+with open('/Users/keyglock/Documents/python/CryptoCurrency/CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv', mode='r') as extra_csv_data:
     extra_reader = csv.reader(extra_csv_data)
     extra_dict_from_tokens_csv = {column[0]:column[2] for column in extra_reader}
 
@@ -56,21 +97,9 @@ extra_cg_token_keys = list(extra_dict_from_tokens_csv.keys())
 extra_cg_token_values = list(extra_dict_from_tokens_csv.values())
 
 
-# delete category 
-with open('CG_Categories.csv', mode='r') as cg_categories_csv_data:
-    reader = csv.reader(cg_categories_csv_data)
-    dict_from_cg_categories_csv = {rows[0]:rows[1] for rows in reader}
 
-cg_categories_keys = dict_from_cg_categories_csv.keys()
-cg_categories_values = list(dict_from_cg_categories_csv.values())
-
-
-
-# if __name__ == "__main__":
-#     print(cg_categories_values)
-#     name_changer()
-    # print(type(extra_dict_from_tokens_csv.items()))
-    # print(type(extra_cg_token_values))
+# if __name__ == '__main__':
+    # name_changer()
     # print(cg_tokens_keys) # bitcoin (ID)
     # print(cg_tokens_values) # btc (symb)
     # print(extra_dict_from_tokens_csv.items())
