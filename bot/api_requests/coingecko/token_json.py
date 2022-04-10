fb7db2c4e0a8beb6de5c2101c880fb763d394acd
Max808S <rayydubb808@gmail.com>
2022-04-10 22:51:31 +0300

    added path for linux

--- bot/api_requests/coingecko/token_json.py
+++ bot/api_requests/coingecko/token_json.py
@@ -24,7 +24,7 @@ def name_changer():
     return result
 
 
-async def categories_list_changer() -> str:
+async def categories_list_changer():
     """
     List all categories and save to CSV with extra named categories
     """
@@ -56,8 +56,8 @@ async def categories_list_changer() -> str:
         return result
 
 
-# with open('/Users/keyglock/Documents/python/CryptoCurrency/CG_categories_list.csv', mode='r') as cg_categories_csv_data:
-with open('/root/TgBot/CryptoCurrency/CG_categories_list.csv', mode='r') as cg_categories_csv_data:
+with open('/Users/keyglock/Documents/python/CryptoCurrency/CG_categories_list.csv', mode='r') as cg_categories_csv_data:
+# with open('/root/TgBot/CryptoCurrency/CG_categories_list.csv', mode='r') as cg_categories_csv_data:
     reader = csv.reader(cg_categories_csv_data)
     dict_from_cg_categories_csv = {rows[0]:rows[1] for rows in reader}
 
@@ -66,8 +66,8 @@ cg_categories_values = list(dict_from_cg_categories_csv.values())
 
 
 # EXAMPLE: tron-eth - tron_eth
-# with open("/Users/keyglock/Documents/python/CryptoCurrency/CG_token_list.csv", mode="r") as csv_data:
-with open("/root/TgBot/CryptoCurrency/CG_token_list.csv", mode="r") as csv_data:
+with open("/Users/keyglock/Documents/python/CryptoCurrency/CG_token_list.csv", mode="r") as csv_data:
+# with open("/root/TgBot/CryptoCurrency/CG_token_list.csv", mode="r") as csv_data:
     reader = csv.reader(csv_data)
     dict_with_token_ids = {rows[0]:rows[1] for rows in reader}
 
@@ -78,8 +78,8 @@ cg_token_extra_name = list(dict_with_token_ids.values())
 
 
 # EXAMPLE: bitcoin - btc
-# with open("/Users/keyglock/Documents/python/CryptoCurrency/CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv", mode="r") as csv_data:
-with open("/root/TgBot/CryptoCurrency/CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv", mode="r") as csv_data:
+with open("/Users/keyglock/Documents/python/CryptoCurrency/CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv", mode="r") as csv_data:
+# with open("/root/TgBot/CryptoCurrency/CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv", mode="r") as csv_data:
     reader = csv.reader(csv_data)
     dict_from_tokens_csv = {rows[0]:rows[1] for rows in reader}
 
@@ -90,8 +90,8 @@ cg_tokens_values = list(dict_from_tokens_csv.values())
 
 
 # EXAMPLE: bitcoin - Bitcoin (name)
-# with open('/Users/keyglock/Documents/python/CryptoCurrency/CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv', mode='r') as extra_csv_data:
-with open('/root/TgBot/CryptoCurrency/CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv', mode='r') as extra_csv_data:
+with open('/Users/keyglock/Documents/python/CryptoCurrency/CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv', mode='r') as extra_csv_data:
+# with open('/root/TgBot/CryptoCurrency/CoinGecko_Token_API_List_-_CoinGecko_Token_API_List.csv', mode='r') as extra_csv_data:
     extra_reader = csv.reader(extra_csv_data)
     extra_dict_from_tokens_csv = {column[0]:column[2] for column in extra_reader}
 
