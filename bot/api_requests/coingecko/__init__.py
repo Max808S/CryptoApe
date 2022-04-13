de5edae4b93614cc550b65cb6be5f66c61a56226
Max808S <rayydubb808@gmail.com>
2022-04-13 23:15:28 +0300

    added improved coin search

--- bot/api_requests/coingecko/__init__.py
+++ bot/api_requests/coingecko/__init__.py
@@ -1,1 +1,4 @@
-from .coingecko import get_categories_list, get_categories_list_data, get_categories_data
+from .coingecko import (
+    get_categories_list, get_categories_list_data, 
+    get_categories_data, cg_searcher
+)
