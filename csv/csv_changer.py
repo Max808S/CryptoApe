b59d5448314fe2135327bd8337f2f6593e6369ee
Max808S <rayydubb808@gmail.com>
2022-05-01 21:10:03 +0300

    change files directory

--- csv/csv_changer.py
+++ csv/csv_changer.py
@@ -5,19 +5,19 @@ from itertools import zip_longest
 
 
 def name_changer():
-    with open("/Users/keyglock/Documents/python/CryptoCurrency/CoinGecko Token API List - CoinGecko Token API List.csv", mode="r") as csv_data:
+    with open("/Users/keyglock/Documents/python/CryptoApe/csv/CoinGecko Token API List - CoinGecko Token API List.csv", mode="r") as csv_data:
         reader = csv.reader(csv_data)    
         token_id_list = [rows[0] for rows in reader]
         # token_symbol_list = [rows[1] for rows in reader] # TODO
         # token_name_list = [rows[2] for rows in reader]
     new_token_list = [word.replace("-", "_") for word in token_id_list]
-    # print("[*] Tokens names with signs '-' changed to '_' successfully!")
+    print("[*] Tokens names with signs '-' changed to '_' successfully!")
 
     data = [token_id_list, new_token_list]
     export_data = zip_longest(*data, fillvalue = '')
-    with open('CG_token_list.csv', 'w', encoding="ISO-8859-1", newline='') as file:
+    with open("/Users/keyglock/Documents/python/CryptoApe/csv/CG_token_list.csv", "w", encoding="ISO-8859-1", newline="") as file:
         write = csv.writer(file)
-        write.writerow(("Id", "Extra"))
+        write.writerow(("ID", "EXTRA"))
         write.writerows(export_data)
     result = "[*] The original and additional names are written to a new CSV file!"
     return result
@@ -47,14 +47,14 @@ async def categories_list_changer():
         data = [categories_id, extra_categories_id, categories_name]
         export_data = zip_longest(*data, fillvalue = '')
 
-        with open('CG_categories_list.csv', 'w', encoding="ISO-8859-1", newline='') as file:
+        with open("/Users/keyglock/Documents/python/CryptoApe/csv/CG_categories_list.csv", "w", encoding="ISO-8859-1", newline="") as file:
             write = csv.writer(file)
-            write.writerow(("ID", "Extra", "Name"))
+            write.writerow(("ID", "EXTRA", "NAME"))
             write.writerows(export_data)
         result = "[*] The original and additional names are written to a new CSV file!"
     return result
 
 
 if __name__ == '__main__':
-    # name_changer()
-    asyncio.run(categories_list_changer())
+    # print(name_changer())
+    asyncio.run(print(categories_list_changer()))
